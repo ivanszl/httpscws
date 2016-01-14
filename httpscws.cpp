26a216e2b9ada4a639e9fe50be7611a495a535c9
ivanszl <ivan.lin.1985@gmail.com>
2016-01-15 01:35:15 +0800

    remove: 删除没有用到的头部文件引用
    remove: 删除没有用到的头部文件引用

--- httpscws.cpp
+++ httpscws.cpp
@@ -11,40 +11,22 @@
  */
 
 extern "C" {
-#include <sys/types.h>
-#include <sys/time.h>
 #include <sys/queue.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <arpa/inet.h>
 #include <fcntl.h>
-#include <time.h>
-#include <sys/ioctl.h>
-#include <errno.h>
-#include <assert.h>
 #include <signal.h>
-#include <stdbool.h>
-#include <wait.h>
 
-#include <signal.h>
-#include <err.h>
 #include <event.h>
 #include <evhttp.h>
 
 #define VERSION "1.0.0"
 #define SCWS_PREFIX "/usr/local/"
 }
-#include "scws.h"
+#include <scws/scws.h>
 
 scws_t g_scws;
 
